b266c885d3d692e997786a059236246f2f760814
seinafassler <seinafaessler@gmail.com>
2025-09-27 09:50:29 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
+
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  // No need to add tailwindcss as a Vite plugin; configure Tailwind via PostCSS.
+  plugins: [tailwindcss()],
 });
