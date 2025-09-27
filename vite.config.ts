bb8126093a92b9c3423206da5ffa1c281a810e82
seinafassler <seinafaessler@gmail.com>
2025-09-27 17:56:07 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
-  plugins: [tailwindcss()],
-});
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
+})
