259f61731a71d91b659c617b3119433070ed63d9
seinafassler <seinafaessler@gmail.com>
2025-09-28 16:20:46 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        red: '#ff4d00',
+        blue: '#007bff',
+        pink: '#ff88e2',
+        green: '#007500',
+        lavender: '#c3c8ff',
+        olive: '#cbc900',
+      },
+    },
+  },
+  plugins: [],
+};
